b871e6c5a8a601e51fc3676fb99ebd499a80dc9b
cmgvc <chloe.gavv@gmail.com>
2024-12-22 17:25:36 -0500

    fix postgres connection url

--- config/database.js
+++ config/database.js
@@ -1,9 +1,19 @@
 const {Sequelize} = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+//     host: process.env.DB_HOST,
+//     dialect: 'postgres',
+// });
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true, // Render databases require SSL
+            rejectUnauthorized: false, // Allow self-signed certificates
+        },
+    },
 });
 
 module.exports = sequelize;
